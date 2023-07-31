dd8ac8062273e5476348657e13517e9acbb2bccc
peninah98 <peninangizwena@gmail.com>
2023-07-31 17:10:14 +0200

    Footer updates

--- footer.js
+++ footer.js
@@ -1,1 +1,7 @@
 console.log("Footer section")
+
+const btn = document.createElement('button')
+btn.textContent='Click me'
+btn.style.padding = '12px 20px'
+btn.style.background='purple'
+btn.style.color= 'white'
